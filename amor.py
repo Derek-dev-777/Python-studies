0408b0e4dd4e6a547525de599cc5ff4dd3f91505
Derek Meneghel <derekmeneghel2004@gmail.com>
2025-04-29 20:00:46 -0300

    Create amor.py

---
+++ amor.py
@@ -0,0 +1,18 @@
+print("osagisagisaigasg")
+
+caixinha = "oi"
+
+print(caixinha)
+
+soma = 4-3
+
+print(soma)
+
+resposta = input("digite algo: ")
+
+print(resposta)
+
+resposta2 = int(input("digite algo"))
+
+if resposta2 > 10:
+    print(" maior que dez")
